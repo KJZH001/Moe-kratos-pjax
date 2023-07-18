c58fc151bc107aa3fc359fcb44b78f1d779d145a
KJZH001 <2802007619@qq.com>
2023-07-18 04:53:59 +0000

    添加 Google AdSense的head标签

--- header.php
+++ header.php
@@ -29,6 +29,9 @@
         if(kratos_option('background_mode')=='image') echo '@media(min-width:768px){.pagination>li>a{background-color:rgba(255,255,255,.8)}.kratos-hentry,.navigation div,.comments-area .comment-list li,#kratos-widget-area .widget,.comment-respond{background-color:rgba(253,253,253,.85)!important}.comment-list .children li{background-color:rgba(255,253,232,.7)!important}body.custom-background{background-image:url('.kratos_option('background_index_image').');background-size:cover;background-attachment:fixed}}';
         if(kratos_option('add_css')) echo kratos_option('add_css'); ?>
     </style>
+    <!-- Google AdSense -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8801574545913691"
+     crossorigin="anonymous"></script>
   </head>
     <?php flush(); ?>
     <body <?php if(kratos_option('background_mode')=='image') echo 'class="custom-background"'; ?>>
