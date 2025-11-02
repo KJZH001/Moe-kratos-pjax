e6bc974c692dd0ca4afa4c8deb3c3ec4a0c76819
KJZH001 <2802007619@qq.com>
2025-11-02 21:45:16 +0800

    判定区域状态

--- inc/multipleDomain.php
+++ inc/multipleDomain.php
@@ -3,6 +3,21 @@
 // By 晓空
 // 创建于2025.11.2
 
-define('KRATOS_SITE_REGION','0.5.9');
+/** 判定区域状态 **/
+/* 返回值有如下二种
+ * REGION_CN        中国区
+ * REGION_GLOBAL    国际区
+ */
+
+// 定义面向中国大陆的域名
+$kratos_china_host="blog.kzmxj.com";
+if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST']==$kratos_china_host) {
+    define('KRATOS_SITE_REGION','REGION_CN');
+}
+else
+{
+    define('KRATOS_SITE_REGION','REGION_GLOBAL');
+}
+
 
 ?>
