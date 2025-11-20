7d5794862fb8cb396a770325a0a9418250f0a973
KJZH001 <2802007619@qq.com>
2025-11-20 13:22:18 +0800

    function kratos_register_sakura_admin_scheme()

--- inc/sakura_admin_theme.php
+++ inc/sakura_admin_theme.php
@@ -21,5 +21,18 @@ function kratos_sakura_dash_scheme($key, $name, $col1, $col2, $col3, $col4, $bas
     );
 }
 
+function kratos_register_sakura_admin_scheme() {
+    kratos_sakura_dash_scheme(
+        'sakura',                   // 配色方案关键字
+        'Sakura',                   // 显示名称
+        '#8fbbb1', '#bfd8d2', '#fedcd2', '#df744a', // 四个主要色调
+        '#e5f8ff', '#ffffff', '#ffffff',           // 基础色、焦点色、当前色
+        // 自定义 CSS 规则：修改菜单文字颜色、设置后台背景图等
+        '#adminmenu .wp-has-current-submenu .wp-submenu a,#adminmenu .wp-has-current-submenu.opensub .wp-submenu a,#adminmenu .wp-submenu a,#adminmenu a.wp-has-current-submenu:focus+.wp-submenu a,#wpadminbar .ab-submenu .ab-item,#wpadminbar .quicklinks .menupop ul li a,#wpadminbar .quicklinks .menupop.hover ul li a,#wpadminbar.nojs .quicklinks .menupop:hover ul li a,.folded #adminmenu .wp-has-current-submenu .wp-submenu a{color:#f3f2f1}body{background-image:url(https://view.moezx.cc/images/2018/01/29/FLOWER.jpg);background-attachment:fixed;}#wpcontent{background:rgba(255,255,255,.6)}'
+    );
+}
+add_action('admin_init', 'kratos_register_sakura_admin_scheme');
+
+
 
 ?>
