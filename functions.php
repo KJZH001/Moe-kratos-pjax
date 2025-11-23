0215201fc1cbce60a978e0cf6c94b0c320e20a3c
KJZH001 <2802007619@qq.com>
2025-11-23 12:57:17 +0800

    提升版本号至0.6.3

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define('KRATOS_VERSION','0.6.2');
+define('KRATOS_VERSION','0.6.3');
 
 require_once(get_template_directory().'/inc/core.php');
 require_once(get_template_directory().'/inc/multipleDomain.php');
